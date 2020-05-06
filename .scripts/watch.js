497a42ac307bc694986368daa6d1c62e66935091
Nick Guerrera <nicholg@microsoft.com>
2020-05-06 11:36:26 -0700

    Improve VS Code experience around watching background compilation
    Set up problem matcher and ensure that error messages put source file paths
    relative to the repo root, which then allows VS Code to navigate to error
    locations correctly.

--- .scripts/watch.js
+++ .scripts/watch.js
@@ -2,8 +2,16 @@ var cp = require('child_process');
 
 require('./for-each').forEachProject((packageName, projectFolder, project) => {
   if (project.scripts && project.scripts.watch) {
-    console.log(`npm run watch {cwd: ${__dirname}/../${projectFolder}}`);
-    const proc = cp.spawn('npm', ['run', 'watch'], { cwd: projectFolder, shell: true, stdio: "inherit" });
+    // NOTE: We deliberately use `tsc --watch --project ${projectFolder}` here
+    // with cwd at the repo root instead of `npm run watch` with cwd at project
+    // folder. This ensures that error messages put source file paths relative
+    // to the repo root, which then allows VS Code to navigate to error
+    // locations correctly.
+    const tsc = `${projectFolder}/node_modules/.bin/tsc`;
+    const args = ['--watch', '--project', projectFolder];
+    console.log(`${tsc} ${args.join(' ')}`);
+
+    const proc = cp.spawn(tsc, args, { cwd: `${__dirname}/../`, shell: true, stdio: "inherit" });
     proc.on("error", (c, s) => {
       console.log(packageName);
       console.error(c);
