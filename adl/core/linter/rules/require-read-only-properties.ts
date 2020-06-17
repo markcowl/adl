2ec8f60325e95fe201d34be88e949b2c77a08eab
Nelson Troncoso Aldas <nd.ta@hotmail.com>
2020-06-17 12:52:10 -0400

    Added require-read-only-properties rule

---
+++ adl/core/linter/rules/require-read-only-properties.ts
@@ -0,0 +1,42 @@
+// This rule suggests three ways to fix an error.
+// The user should be able to choose a fix using code actions.
+
+import { Rule } from '../rule';
+export default <Rule>{
+  runOn: 'edit',
+  meta: {
+    name: 'required-read-only-properties',
+    code: 'R2056',
+    type: 'error',
+    description: 'A model property cannot be both readOnly and required. A readOnly property is something that the server sets when returning the model object while required is a property to be set when sending it as a part of the request body.',
+    documentationUrl: 'https://github.com/Azure/azure-rest-api-specs/blob/master/documentation/openapi-authoring-automated-guidelines.md#r2056-requiredreadonlyproperties',
+    category: 'SDK Error'
+  },
+  onProperty: (model, property) => {
+    if (property.readOnly && property) {
+      return [
+        {
+          message: 'Set required = false.',
+          fix: () => {
+            property.readOnly = false;
+          }
+        },
+        {
+          message: 'Set remove = false',
+          fix: () => {
+            property.required = false;
+          }
+        },
+        {
+          message: 'Set required = false and readonly = false.',
+          fix: () => {
+            property.required = false;
+            property.readOnly = false;
+          }
+        }
+      ];
+    }
+
+    return;
+  }
+};
