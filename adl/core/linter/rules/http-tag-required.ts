10cad0ee8949f84e0a879946884305407a6290e2
Nelson Troncoso Aldas <nd.ta@hotmail.com>
2020-06-23 10:38:50 -0400

    Added extra rules

---
+++ adl/core/linter/rules/http-tag-required.ts
@@ -0,0 +1,31 @@
+import { getTagValue, setTag } from '../../support/doc-tag';
+import { Rule } from '../rule';
+export default <Rule>{
+  runOn: 'edit',
+  meta: {
+    name: 'http-tag-required',
+    code: 'R1009',
+    type: 'warning',
+    description: 'It is required to provide the operation and path using the http tag.',
+    documentationUrl: 'PLACEHOLDER',
+    category: 'SDK Warning'
+  },
+  onOperation: (model, operation) => {
+    const tag = getTagValue(operation, 'http');
+    if (tag !== undefined) {
+      return {
+        message: `The operation ${operation.name} has the http tag missing.`,
+        suggestion: [
+          {
+            description: 'Provide an http tag.',
+            fix: () => {
+              setTag(operation, 'http', '@http GET /foo/bar/bin/baz');
+            }
+          }
+        ]
+      };
+    }
+
+    return;
+  }
+};
