8bf89a54489338af79d16ef9df19a9b29617b7a2
Brian Terlson <brian.terlson@microsoft.com>
2021-02-11 13:40:08 -0800

    remove chaff from vis.js

--- packages/adl/samples/visibility/vis.js
+++ packages/adl/samples/visibility/vis.js
@@ -6,20 +6,7 @@ export function v(c, t, v) {
   visibility.set(t, v);
 }
 
-const dm = new Map();
-export function d(c, t, v) {
-  dm.set(t, v);
-}
-
-export function inspectD(c, t) {
-  console.log('docs for ' + t.name);
-  for (const [name, prop] of t.properties) {
-    console.log(name + ': ' + dm.get(prop));
-  }
-}
-
 export function wv(c, t, v) {
-  console.log('running wv on ', t);
   const filter = (_, prop) => {
     const vis = getVisibility(prop);
     return vis && vis !== v;
